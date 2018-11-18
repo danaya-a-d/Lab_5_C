16adc61a2cae37f6b155f62f522dd875cc9c2d92
Danaya <danaya.a.d@yandex.ua>
2018-11-18 21:01:02 -0100

    ...

--- Lab_5_C/Form1.Designer.cs
+++ Lab_5_C/Form1.Designer.cs
@@ -28,13 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnGetUserName = new System.Windows.Forms.Button();
+            this.lblGetUserName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnGetUserName
+            // 
+            this.btnGetUserName.Location = new System.Drawing.Point(47, 44);
+            this.btnGetUserName.Name = "btnGetUserName";
+            this.btnGetUserName.Size = new System.Drawing.Size(75, 23);
+            this.btnGetUserName.TabIndex = 0;
+            this.btnGetUserName.Text = "Получить!";
+            this.btnGetUserName.UseVisualStyleBackColor = true;
+            this.btnGetUserName.Click += new System.EventHandler(this.btnGetUserName_Click);
+            // 
+            // lblGetUserName
+            // 
+            this.lblGetUserName.AutoSize = true;
+            this.lblGetUserName.Location = new System.Drawing.Point(167, 49);
+            this.lblGetUserName.Name = "lblGetUserName";
+            this.lblGetUserName.Size = new System.Drawing.Size(109, 13);
+            this.lblGetUserName.TabIndex = 1;
+            this.lblGetUserName.Text = "Имя пользователя: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(619, 90);
+            this.Controls.Add(this.lblGetUserName);
+            this.Controls.Add(this.btnGetUserName);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnGetUserName;
+        private System.Windows.Forms.Label lblGetUserName;
     }
 }
 
--- WinAPIClass.cs
+++ WinAPIClass.cs
@@ -1,35 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 using System.Runtime.InteropServices;
-
+using System.Text;
 
 namespace Lab_5_C
 {
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
-            InitializeComponent();
-        }
-
-        private void btnGetUserName_Click(object sender, System.EventArgs e)
-        {
-            StringBuilder builder = new StringBuilder(100);
-            uint size = 100;
-            WinAPIClass.GetUserNameEx((int)WinAPIClass.NameFormat.NameSamCompatible, builder, ref size);
-            lblGetUserName.Text = String.Format("Имя пользователя: {0}", builder.ToString());
-        }
-
-    }
-
-    internal class WinAPIClass
+    public class WinAPIClass
     {
         #region Получение имени текущего пользователя
         /// <summary>
@@ -63,3 +38,4 @@ namespace Lab_5_C
 
     }
 }
+
